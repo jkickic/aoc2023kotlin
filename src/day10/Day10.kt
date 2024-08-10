93ce6eb7253708e1316adf9ab88b99108aa1af74
mzajac <pentacor.zajac@extaccount.com>
2024-08-10 18:56:17 +0200

    day10 init

---
+++ src/day10/Day10.kt
@@ -0,0 +1,5 @@
+package day10
+
+fun test() {
+    println("Hello World!")
+}
---
+++ test/day10/Day10KtTest.kt
@@ -0,0 +1,11 @@
+package day10
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day10KtTest {
+
+    @org.junit.jupiter.api.Test
+    fun test1() {
+        test()
+    }
+}
